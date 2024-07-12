93c2d5e0080d0bbe4abb58caf3ff2e62a543cd44
Ivan Munandar <3332190035@untirta.ac.id>
2024-07-12 16:18:15 +0700

    add image for my beautica page

--- public/images/images.tsx
+++ public/images/images.tsx
@@ -1,11 +1,19 @@
-import myg_logo from './myg _logo.jpg';
-import image_login from './image_login.png';
+import myg_logo from './myg_logo.png';
 import login_img from './login_img.png';
+import my_Beauty_logo from './my_beauty_1.png';
+import myBeautica_image1 from './myBeautica_image1.png';
+import myBeautica_image2 from './myBeautica_image2.png';
+import myBeautica_image3 from './myBeautica_image3.png';
+import myBeautica_image4 from './myBeautica_image4.png';
 
 const images = {
     myg_logo,
-    image_login,
-    login_img
+    login_img,
+    my_Beauty_logo,
+    myBeautica_image1,
+    myBeautica_image2,
+    myBeautica_image3,
+    myBeautica_image4
 };
 
 export default images;
