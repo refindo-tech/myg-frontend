b95362b234a0b70f1fd0539ceffb6d08ee9697de
Hernando Dio Palma <hernando.diopalma@gmail.com>
2024-07-23 19:12:25 +0700

    Add useProducts and useTestimonies hooks for state management.

---
+++ src/hooks/mya/useProducts.ts
@@ -0,0 +1,26 @@
+// SWR + Axios, axios berfungsi untuk mengambil data dari API, sedangkan SWR berfungsi untuk mengelola state dari data yang diambil dari API.
+
+import useSWR from 'swr';
+import Product from '../../types/mya/product';
+import ProductService from '@/lib/mya/productService';
+
+class useProducts {
+  static all(params: { limit: number }) {
+    const { data, error } = useSWR<Product[], Error>(['products', params], () => ProductService.fetchProducts(params));
+    return {
+      data: data || [],
+      isLoading: !error && !data,
+      isError: error,
+    };
+  }
+  static byId(id: number) {
+    const { data, error } = useSWR<Product, Error>(`product/${id}`, () => ProductService.fetchProductById(id));
+    return {
+      data: data,
+      isLoading: !error && !data,
+      isError: error,
+    };
+  }
+}
+
+export default useProducts;
---
+++ src/hooks/useTestimonies.ts
@@ -0,0 +1,26 @@
+// SWR + Axios, axios berfungsi untuk mengambil data dari API, sedangkan SWR berfungsi untuk mengelola state dari data yang diambil dari API.
+
+import useSWR from 'swr';
+import Testimony from '@/types/testimony';
+import TestimonyService from '@/lib/testimonyService';
+
+class useTestimonies {
+  static all(params: { limit: number }) {
+    const { data, error } = useSWR<Testimony[], Error>(['products', params], () => TestimonyService.fetchTestimonies(params));
+    return {
+      data: data || [],
+      isLoading: !error && !data,
+      isError: error,
+    };
+  }
+  static byId(id: number) {
+    const { data, error } = useSWR<Testimony, Error>(`product/${id}`, () => TestimonyService.fetchTestimonyById(id));
+    return {
+      data: data,
+      isLoading: !error && !data,
+      isError: error,
+    };
+  }
+}
+
+export default useTestimonies;
