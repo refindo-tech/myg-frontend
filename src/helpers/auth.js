81442a4b89df27309df66c71e4a90a2cdf3db680
Hernando Dio Palma <hernando.diopalma@gmail.com>
2024-07-31 13:00:47 +0700

    add isAuthenticated helper function for authentication

---
+++ src/helpers/auth.js
@@ -0,0 +1,6 @@
+
+//check refreshToken in cookie for authentication
+export const isAuthenticated = () => {
+    const token = sessionStorage.getItem('accessToken') || null;
+    return token ? true : false;
+};
