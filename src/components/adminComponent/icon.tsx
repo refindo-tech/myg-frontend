f9e678282345b130764e5c104ade8cc2ff3bd079
Ivan Munandar <3332190035@untirta.ac.id>
2024-08-29 19:02:51 +0700

    create sidebar and navbar for admin page

---
+++ src/components/adminComponent/icon.tsx
@@ -0,0 +1,172 @@
+import React from 'react';
+
+interface IconProps {
+    size?: number;
+    fill?: string;
+    width?: number;
+    height?: number;
+}
+
+export const HomeIcon: React.FC<IconProps> = ({ size = 24, fill = "currentColor", width, height, ...props }) => (
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="currentColor"
+        className="size-6"
+        {...props}
+    >
+        <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+        />
+    </svg>
+);
+
+export const UserIcon: React.FC<IconProps> = ({ size = 24, fill = "currentColor", width, height, ...props }) => (
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="currentColor"
+        className="size-6"
+        {...props}
+    >
+        <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M15 19.128a9.38 9.38 0 0 0 2.625.372 9.337 9.337 0 0 0 4.121-.952 4.125 4.125 0 0 0-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 0 1 8.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0 1 11.964-3.07M12 6.375a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0Zm8.25 2.25a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z"
+        />
+    </svg>
+);
+
+export const BookIcon: React.FC<IconProps> = ({ size = 24, fill = "currentColor", width, height, ...props }) => (
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="currentColor"
+        className="size-6"
+        {...props}
+    >
+        <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M12 6.042A8.967 8.967 0 0 0 6 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 0 1 6 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 0 1 6-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0 0 18 18a8.967 8.967 0 0 0-6 2.292m0-14.25v14.25"
+        />
+    </svg>
+);
+
+export const KonsulIcon: React.FC<IconProps> = ({ size = 24, fill = "currentColor", width, height, ...props }) => (
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="currentColor"
+        className="size-6"
+        {...props}
+    >
+        <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z"
+        />
+    </svg>
+);
+
+export const ProductIcon: React.FC<IconProps> = ({ size = 24, fill = "currentColor", width, height, ...props }) => (
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="currentColor"
+        className="size-6"
+        {...props}
+    >
+        <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="m20.25 7.5-.625 10.632a2.25 2.25 0 0 1-2.247 2.118H6.622a2.25 2.25 0 0 1-2.247-2.118L3.75 7.5M10 11.25h4M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125Z"
+        />
+    </svg>
+);
+
+export const PenjualanIcon: React.FC<IconProps> = ({ size = 24, fill = "currentColor", width, height, ...props }) => (
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="currentColor"
+        className="size-6"
+        {...props}
+    >
+        <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"
+        />
+    </svg>
+);
+
+export const LayananIcon: React.FC<IconProps> = ({ size = 24, fill = "currentColor", width, height, ...props }) => (
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="currentColor"
+        className="size-6"
+        {...props}
+    >
+        <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M4.098 19.902a3.75 3.75 0 0 0 5.304 0l6.401-6.402M6.75 21A3.75 3.75 0 0 1 3 17.25V4.125C3 3.504 3.504 3 4.125 3h5.25c.621 0 1.125.504 1.125 1.125v4.072M6.75 21a3.75 3.75 0 0 0 3.75-3.75V8.197M6.75 21h13.125c.621 0 1.125-.504 1.125-1.125v-5.25c0-.621-.504-1.125-1.125-1.125h-4.072M10.5 8.197l2.88-2.88c.438-.439 1.15-.439 1.59 0l3.712 3.713c.44.44.44 1.152 0 1.59l-2.879 2.88M6.75 17.25h.008v.008H6.75v-.008Z"
+        />
+    </svg>
+);
+
+
+export const TestimonialIcon: React.FC<IconProps> = ({ size = 24, fill = "currentColor", width, height, ...props }) => (
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="currentColor"
+        className="size-6"
+        {...props}
+    >
+        <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M20.25 8.511c.884.284 1.5 1.128 1.5 2.097v4.286c0 1.136-.847 2.1-1.98 2.193-.34.027-.68.052-1.02.072v3.091l-3-3c-1.354 0-2.694-.055-4.02-.163a2.115 2.115 0 0 1-.825-.242m9.345-8.334a2.126 2.126 0 0 0-.476-.095 48.64 48.64 0 0 0-8.048 0c-1.131.094-1.976 1.057-1.976 2.192v4.286c0 .837.46 1.58 1.155 1.951m9.345-8.334V6.637c0-1.621-1.152-3.026-2.76-3.235A48.455 48.455 0 0 0 11.25 3c-2.115 0-4.198.137-6.24.402-1.608.209-2.76 1.614-2.76 3.235v6.226c0 1.621 1.152 3.026 2.76 3.235.577.075 1.157.14 1.74.194V21l4.155-4.155"
+        />
+    </svg>
+);
+
+export const LogoutIcon: React.FC<IconProps> = ({ size = 24, fill = "currentColor", width, height, ...props }) => (
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="currentColor"
+        className="size-6"
+        {...props}
+    >
+        <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M15.75 9V5.25A2.25 2.25 0 0 0 13.5 3h-6a2.25 2.25 0 0 0-2.25 2.25v13.5A2.25 2.25 0 0 0 7.5 21h6a2.25 2.25 0 0 0 2.25-2.25V15m3 0 3-3m0 0-3-3m3 3H9"
+        />
+    </svg>
+);
+
---
+++ src/components/adminComponent/navbar/navbar.tsx
@@ -0,0 +1,118 @@
+import React, { ChangeEvent } from "react";
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  Link,
+  Input,
+  Dropdown,
+  DropdownTrigger,
+  DropdownMenu,
+  DropdownItem,
+  Avatar,
+  Button,
+  Image,
+} from "@nextui-org/react";
+import { useRouter } from "next/navigation";
+import icons from "@/components/icons/icon";
+import images from "../../../../public/images/images";
+import useAuthCheck from "@/hooks/common/auth"; // Pastikan path ini benar
+
+type NavbarProps = {
+  userData: {
+    email: string;
+    profilePicture: string | null;
+    fullName: string;
+  } | null;
+  onLogout: () => void;
+};
+
+const NavbarComponent: React.FC<NavbarProps> = ({
+  userData,
+  onLogout,
+}) => {
+  const isLogged = useAuthCheck();
+  const router = useRouter();
+
+  const handleLogin = () => {
+    // console.log("Login clicked");
+    router.push("/login");
+  };
+
+  return (
+    <div className="relative flex flex-col flex-1 overflow-y-auto overflow-x-hidden">
+      <Navbar isBordered className="bg-white py-2 inline-flex mx-auto gap-20">
+
+        <NavbarContent as="div" className="items-center" justify="end">
+
+          <Dropdown
+            placement="bottom-end"
+            className="hidden xl:flex items-center gap-2 font-openSans text-zinc"
+          >
+            <DropdownTrigger>
+              <div className="xl:flex items-center hidden gap-2">
+                <Avatar
+                  showFallback
+                  name={userData?.fullName || "Jane Doe"}
+                  src={
+                    userData?.profilePicture ||
+                    "https://api.dicebear.com/9.x/thumbs/svg?seed=Felix"
+                  }
+                  className="bg-ungu2 text-white font-inter"
+                />
+                {!isLogged ? ( // Jika pengguna belum login, tampilkan tombol login
+                  <Button isIconOnly variant="light" onClick={handleLogin}>
+                    Login
+                  </Button>
+                ) : (
+                  <p className="font-semibold hidden">
+                    {userData?.email || "zoey@example.com"}
+                  </p>
+                )}
+              </div>
+            </DropdownTrigger>
+            {isLogged && (
+              <DropdownMenu aria-label="Profile Actions" variant="flat">
+                <DropdownItem key="profile" className="h-14 gap-2">
+                  <p className="font-semibold">Signed in as</p>
+                  <p className="font-semibold">
+                    {userData?.email || "zoey@example.com"}
+                  </p>
+                </DropdownItem>
+                {/* <DropdownItem key="settings">My Profile</DropdownItem> */}
+                <DropdownItem key="logout" color="danger" onClick={onLogout}>
+                  Log Out
+                </DropdownItem>
+              </DropdownMenu>
+            )}
+          </Dropdown>
+
+          {isLogged && (
+            <Dropdown className="md:hidden">
+              <DropdownTrigger>
+                <Button isIconOnly variant="light" className="md:hidden">
+                  <icons.MenuIcon />
+                </Button>
+              </DropdownTrigger>
+              <DropdownMenu aria-label="Profile Actions" variant="flat">
+                <DropdownItem key="profile" className="h-14 gap-2">
+                  <p className="font-semibold">Signed in as</p>
+                  <p className="font-semibold">
+                    {userData?.email || "zoey@example.com"}
+                  </p>
+                </DropdownItem>
+                <DropdownItem key="settings">My Profile</DropdownItem>
+                <DropdownItem key="logout" color="danger" onClick={onLogout}>
+                  Log Out
+                </DropdownItem>
+              </DropdownMenu>
+            </Dropdown>
+          )}
+        </NavbarContent>
+      </Navbar>
+    </div>
+  );
+};
+
+export default NavbarComponent;
